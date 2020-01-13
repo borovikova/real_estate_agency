4d247b1c4bfa67d899562c037dd3304728aa83c6
Ekaterina <borovikova@zoho.eu>
2020-01-13 07:55:35 +0100

    Add search field to admin panel

--- property/admin.py
+++ property/admin.py
@@ -2,5 +2,7 @@ from django.contrib import admin
 
 from .models import Flat
 
+class FlatSearch(admin.ModelAdmin):
+    search_fields = ('town','owner','address')
 
-admin.site.register(Flat)
+admin.site.register(Flat, FlatSearch)
